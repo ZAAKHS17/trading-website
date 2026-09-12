7a9c6312138965466107ae1fc82b31c5f58d7c2b
ZAAKHS17 <101148607+ZAAKHS17@users.noreply.github.com>
2026-09-12 23:04:18 +0100

    chore: pin Next/TS tooling configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: ["./src/app/**/*.{ts,tsx}", "./src/components/**/*.{ts,tsx}"],
-  darkMode: 'class',
+  darkMode: "class",
   theme: {
     extend: {},
   },
